97ca97a032d3be40a1e7d820870180fc24435823
MayraLopez13 <31629639+MayraLopez13@users.noreply.github.com>
2017-11-14 09:36:46 -0600

    Add files via upload

---
+++ TAREA_21/letras.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+
+void letram () {
+	printf("M     M\n");
+	printf("M  M  M\n");
+	printf("M  M  M\n");
+	printf("M  M  M\n");
+	printf("M     M\n\n");
+}
+
+void letraa () {
+	printf("AAAAAA\n");
+	printf("A    A\n");
+	printf("A AA A\n");
+	printf("A    A\n");
+	printf("A    A\n\n");
+}
+
+void letray () {
+	printf("Y    Y\n");
+	printf("Y    Y\n");
+	printf(" Y Y  \n ");
+	printf(" Y   \n");
+	printf("  Y   \n\n");
+}
+
+void letrar () {
+	printf("R R  R\n");
+	printf("R    R\n");
+	printf("R R R \n");
+	printf("R   R  \n");
+	printf("R   R  \n\n");
+}
---
+++ TAREA_21/tarea21.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "letras.h"
+void marco()
+{
+  printf("----------------------------------------------------------------\n");
+  printf("_  \n");
+  printf("_  \n"); letram ();                                                            
+  printf("_  \n"); letraa ();                                                                                     
+  printf("_  \n"); letray ();                                                           
+  printf("_  \n"); letrar ();
+  printf("_  \n"); letraa ();
+  printf("----------------------------------------------------------------\n");
+}
+
+
+int main () {
+
+	marco();
+			
+}
