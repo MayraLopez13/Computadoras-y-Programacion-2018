5eb14e8b3427e3cc7f6dbdd998e7dddad4c8966b
MayraLopez13 <31629639+MayraLopez13@users.noreply.github.com>
2017-11-22 00:22:09 -0600

    Add files via upload

---
+++ tarea23/programa1b.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+struct alumnos 
+     {
+     	 int edad;
+     	 char nombre[60];
+     	 char genero;
+     	 char carrera[25];
+     	 char nCuenta[10];
+     	 
+	 };
+
+typedef struct alumnos ALUMNO; //alias a la estructura
+	 
+int main (int argc, char *argv[]) {
+	
+	ALUMNO var1; //se manda a llamar el alias
+	
+	printf("Introduce la edad: ");
+	scanf("%d",&var1.edad);
+	
+	printf("Introduce el nombre: ");
+	fflush(stdin);
+	gets(var1.nombre);
+	
+	printf("Edad:%d nombre:%s",var1.edad, var1.nombre);
+	
+	return 0;
+}
